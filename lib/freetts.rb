06a0625e337bdc4fb873b1bc783655612cfb1b7e
Chris Kottom <chris@chriskottom.com>
2011-09-18 19:03:46 +0200

    moved contents of class_methods.rb directly into main FreeTTS module source

--- lib/freetts.rb
+++ lib/freetts.rb
@@ -3,5 +3,14 @@ $:.unshift(File.dirname(__FILE__))
 require "java"
 require "vendor/freetts.jar"
 
-require "freetts/class_methods"
 require "freetts/voice"
+
+module FreeTTS
+  @voice = Voice.for_name(Voice::DEFAULT_NAME)
+  class << self; attr_accessor :voice; end
+
+
+  def self.speak(speakable)
+    self.voice.speak(speakable)
+  end
+end
