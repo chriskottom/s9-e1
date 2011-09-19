23a6ff0049664981c9d0367157b6e3530c36923e
Chris Kottom <chris@chriskottom.com>
2011-09-19 17:00:15 +0200

    added test case for FreeTTS class methods

---
+++ test/freetts_test.rb
@@ -0,0 +1,19 @@
+$:.unshift File.join(File.dirname(__FILE__), "../lib")
+
+require "freetts"
+require "test/unit"
+
+
+class FreeTTSTest < Test::Unit::TestCase
+  def test_setting_current_voice
+    start_voice = FreeTTS.voice
+    all_voices = FreeTTS::Voice.all
+
+    new_voice_name = all_voices.reject { |name| name == start_voice.name }.first
+    FreeTTS.voice = FreeTTS::Voice.for_name(new_voice_name)
+    new_voice = FreeTTS.voice
+
+    assert_not_nil(new_voice)
+    assert_equal(new_voice_name, new_voice.name)
+  end
+end
