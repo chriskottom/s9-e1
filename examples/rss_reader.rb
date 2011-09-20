bc1d261961c3a195b476afcc4a22c42c17a4b469
Chris Kottom <chris@chriskottom.com>
2011-09-20 08:57:36 +0200

    handling capture of title content for both RSS and Atom

--- examples/rss_reader.rb
+++ examples/rss_reader.rb
@@ -5,14 +5,19 @@ require "rss"
 require "freetts"
 
 
-feed_url = "http://feeds.feedburner.com/RailsInside"
+feed_url = "http://feeds.feedburner.com/RubyInside"
 
 uri = URI.parse(feed_url)
 
 feed_items = RSS::Parser.parse(uri.read, false).items.first(5)
 
 feed_items.each do |item|
-  title = item.title.content
+  title = begin
+    item.title.content    # works for RSS:Atom feeds
+  rescue NoMethodError
+    item.title.to_s       # works for standard RSS
+  end
+
   puts title
   FreeTTS.speak(title)
 end
