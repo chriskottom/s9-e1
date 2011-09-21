a7a23fffe5da672963c8bd9551d1d75f0539d971
Chris Kottom <chris@chriskottom.com>
2011-09-21 08:39:41 +0200

    removal of method_missing from Voice, also inclusion of class methods in class << self block

--- lib/freetts/voice.rb
+++ lib/freetts/voice.rb
@@ -9,9 +9,25 @@ module FreeTTS
     MODIFIABLE_ATTRIBUTES = %w( duration_stretch pitch pitch_range pitch_shift
                                 rate volume )
 
+    class << self
+      attr_reader :voice_manager
+
+      def all
+        voice_manager.get_voices.map { |voice| voice.get_name }
+      end
+
+      def for_name(voice_name)
+        voice = voice_manager.get_voice(voice_name)
+        if voice
+          voice.allocate
+          Voice.new(voice)
+        else
+          raise "No voice found for name \"#{ voice_name }\""
+        end
+      end
+    end
 
     @voice_manager = com.sun.speech.freetts.VoiceManager.get_instance
-    class << self; attr_reader :voice_manager; end
 
     ACCESSIBLE_ATTRIBUTES.each do |attribute|
       ruby_method = attribute.to_sym
@@ -31,37 +47,6 @@ module FreeTTS
 
     def speak(saying)
       @voice_impl.speak(saying)
-    end
-
-    def method_missing(method, *args, &block)
-      method = method.to_s
-      java_method_name = case method
-      when /\A(.*)=\Z/
-        "set_#{ $1 }"
-      when /\A(.*)\Z/
-        "get_#{ $1 }"
-      end
-
-      if java_method_name and @voice_impl.respond_to?(java_method_name.to_sym)
-        return @voice_impl.send(java_method_name.to_sym, *args)
-      else
-        error_message = "undefined method #{ method } for #{ self }:Voice"
-        raise NoMethodError.new(error_message)
-      end
-    end
-
-    def self.all
-      voice_manager.get_voices.map { |voice| voice.get_name }
-    end
-
-    def self.for_name(voice_name)
-      voice = voice_manager.get_voice(voice_name)
-      if voice
-        voice.allocate
-        Voice.new(voice)
-      else
-        raise "No voice found for name \"#{ voice_name }\""
-      end
     end
   end
 end
